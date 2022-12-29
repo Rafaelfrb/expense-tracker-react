d2955293ce4ba383ac9931658167303d273e0e70
Rafael Freitas <rafael.frb@hotmail.com>
2022-12-29 21:52:22 +0000

    Styling ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div className="expense-item">
+            <div>March 27 2023</div>
+            <div className="expense-item__description">
+                <h2>Car insurance</h2>
+                <div className="expense-item__price">$279.90</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem
